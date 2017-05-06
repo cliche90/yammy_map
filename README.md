1aeab4155e46280e25748a6f84b59caaff56fa27
snacky <cliche90@naver.com>
2017-05-06 19:47:17 +0900

    modify file name, contents

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Yammy Map!!
 
-## 협업 #01
-    git pull
-    git merge [branch name]
+## # Collaborators
+
+- Repository에 Collaborator 추가
+    > repository - setting - collaborators 탭 - 협업 인원 검색 - Add collaborator
+
+- Branch merge
+
+        git pull
+        git merge [branch name]
+
+    > 각 <strong>contributor</strong>의 branch에 업로드 된 내용을 pull 하고, 브랜치를 merge
+
+## # OpenLayers
---
+++ server.js
@@ -0,0 +1,18 @@
+var $ = require("jquery");
+
+var express = require('express');
+var request = require('request');
+var path = require('path');
+path.dirname('/views');
+
+var app = express();
+app.use(express.static(__dirname + "/static"));
+app.set('view engine', 'jade');
+app.set('views', 'views');
+
+app.get('/yammy', (req, res) => {
+    console.log('yammy')
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+app.listen(3000, () => { console.log("connected!!"); });
