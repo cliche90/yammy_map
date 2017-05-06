59896221327da408e655f6d96bb59a4322f4d1af
snacky <cliche90@naver.com>
2017-05-06 17:19:19 +0900

    add path renderer

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var request = require('request');
+var path = require('path');
+path.dirname('/views');
 
 var app = express();
 app.use(express.static(__dirname + "./static"));
@@ -7,9 +9,8 @@ app.set('view engine', 'jade');
 app.set('views', 'views');
 
 app.get('/yammy', (req, res) => {
-    
     console.log('yammy')
-    res.render('index');
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
 app.listen(3000, () => { console.log("connected!!"); });
